ec7aafa042503537d426ff2e0d7bc8805cf92e50
ngarrison39 <ngarrison39@aol.com>
2015-11-11 13:03:18 -0800

    committing in case of laptop failure.  Structured MyCompiler for .mkd file use

--- src/edu/towson/cis/cosc455/ngarrison/project1/implementation/MyCompiler.java
+++ src/edu/towson/cis/cosc455/ngarrison/project1/implementation/MyCompiler.java
@@ -5,31 +5,40 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class MyCompiler {
+	public static String completeFile;
+	/*
 	//public static String completeFile = "#BEGIN ^header here^ $DEF = @%* afga @ % * ~ { ] [ } [erw] {erg } ; gc*gc%gc@gc(gc;gc^gc**gc-gc=gc?  this should be text **bold here** #END";
 	public static String completeFile = "#BEGIN ^<simple  test>^ { ** bold here ** } *italics here* plain text @(http://www.help.com) #END";
 	//public static String completeFile = "#BEGIN ^<simple  test>^ { ** bold here ** } *italics here* plain text";
+
 	public static void main(String args[]){
 		System.out.println(completeFile + " --> completeFile at line 11 MyComp");
 		MyLexicalAnalyzer prime = new MyLexicalAnalyzer(completeFile);
 		MySyntaxAnalyzer  build = new MySyntaxAnalyzer();
-		/*
-		 * endsWith()
+	}
+	 */
+	public static void main(String args[]){
 		String fileName = "";
 		Scanner input = new Scanner(System.in);
-		System.out.println("Enter the filename to be used with extension .mkd:");
+		System.out.println("Enter the filename to be used with extension .mkd :");
 		if(input.hasNext()){
-		fileName = input.next();
+			fileName = input.next();
+		}
+		while(!fileName.endsWith(".mkd")){
+			System.out.println("The filename must be of extension .mkd");
+			System.out.println("Please re-enter the filename:");
+			fileName = input.next();
 		}
 		try {
-			Scanner file = new Scanner(new File(fileName));
+			Scanner file = new Scanner(new File(fileName));  // need to put fileName in quotes???
 			while(file.hasNextLine()){
 				completeFile += file.nextLine(); 
 			}
 			input.close();
 			file.close();
 			System.out.println(completeFile + " --> completeFile at line 11 MyComp");
-			MyLexicalAnalyzer createTokens = new MyLexicalAnalyzer(completeFile);
-
+			MyLexicalAnalyzer prime = new MyLexicalAnalyzer(completeFile);
+			MySyntaxAnalyzer  build = new MySyntaxAnalyzer();
 
 		} catch (FileNotFoundException e) {
 			System.out.println("The file " + fileName + " was not found.");
@@ -37,7 +46,5 @@ public class MyCompiler {
 			System.exit(1);
 			//e.printStackTrace();
 		}
-
-		 */
 	}
 }
